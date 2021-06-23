2f472318905e7077bef671a4ec3cc9830008746b
unity <unityoxb@gmail.com>
2021-06-23 16:09:57 +0800

    add CROS

--- v1/main.py
+++ v1/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 import uvicorn
 from routers import AuthorsRouter, StagesRouter
+from fastapi.middleware.cors import CORSMiddleware
 
 
 app = FastAPI(
@@ -9,8 +10,24 @@ app = FastAPI(
     description="Scifanchain的数据API接口，为Client端提供数据和应用逻辑， 并且提供Websocket服务。本接口开源并面向公众开放，任何第三方客户端皆可使用本接口获取Scifanchain的内容数据。",
     version="0.1.0",
     docs_url="/docs", 
-    redoc_url="/redoc",
+    redoc_url="",
 )
+
+origins = [
+    "http://scifanchain.com",
+    "http://www.scifanchain.com",
+    "https://scifanchain.com",
+    "https://www.scifanchain.com",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 app.include_router(AuthorsRouter.router)
 app.include_router(StagesRouter.router)
