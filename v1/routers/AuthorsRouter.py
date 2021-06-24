ea1695bce3178b337e52e1c62b78c78dc615fa12
unity <unityoxb@gmail.com>
2021-06-24 12:27:38 +0800

    update author

--- v1/routers/AuthorsRouter.py
+++ v1/routers/AuthorsRouter.py
@@ -51,7 +51,7 @@ async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(
     access_token = crud.create_access_token(
         data={"sub": user.username}, expires_delta=access_token_expires
     )
-    return {"access_token": access_token, "token_type": "bearer"}
+    return {"access_token": access_token, "token_type": "bearer", "exp": access_token_expires}
 
 
 
