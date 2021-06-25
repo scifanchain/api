c8587fb943c8d432bdd9f40176bbf6a17ce43a8a
unity <unityoxb@gmail.com>
2021-06-25 10:30:18 +0800

    update

--- v1/routers/StagesRouter.py
+++ v1/routers/StagesRouter.py
@@ -2,7 +2,7 @@ from datapools import crud, schemas
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm.session import Session
 
-from config.dpds import get_token_header
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 
 from datapools.database import get_db
 
@@ -14,7 +14,10 @@ router = APIRouter(
 
 )
 
-fake_items_db = {"plumbus": {"name": "Plumbus"}, "gun": {"name": "Portal Gun"}}
+
+@router.post("/create_stage/", response_model=schemas.Stage)
+async def create_stage(stage: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db), author: schemas.Author = Depends(crud.get_current_user)):
+    return crud.create_stage(db=db, stage=stage, author=author)
 
 
 @router.get("/")
@@ -23,14 +26,11 @@ async def read_stages(skip: int = 0, limit: int = 100, db: Session = Depends(get
     return stages
 
 @router.get("/{stage_id}")
-async def read_stages(stage_id:int, db: Session = Depends(get_db)):
+async def read_stages(stage_id: int, db: Session = Depends(get_db)):
     stage = crud.get_stage(stage_id, db)
     return stage
 
 
-@router.post("/create_stage/", response_model=schemas.Stage)
-async def create_stage(stage: schemas.StageCreate, db: Session = Depends(get_db), author: schemas.Author = Depends(crud.get_current_user)):
-    return crud.create_stage(db=db, stage=stage, author=author)
 
 
 
