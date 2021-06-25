a983c104a194f4c017d69ab783aecca191134b42
unity <unityoxb@gmail.com>
2021-06-25 19:47:27 +0800

    update

--- v1/routers/StagesRouter.py
+++ v1/routers/StagesRouter.py
@@ -6,31 +6,27 @@ from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 
 from datapools.database import get_db
 
-router = APIRouter(
-    prefix="/stages",
-    tags=["stages"],
-    # dependencies=[Depends(get_token_header)],
-    responses={404: {"description": "Not found"}},
+router = APIRouter()
 
-)
 
-@router.post("/create_stage/", response_model=schemas.Stage)
+@router.post("/stages/create_stage/", response_model=schemas.Stage, tags=["authors"])
 async def create_stage(stage: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db), author: schemas.Author = Depends(crud.get_current_user)):
     return crud.create_stage(stage=stage, db=db, author=author)
 
 
-@router.post("/test/", response_model=schemas.Test)
+@router.post("/stages/test/", response_model=schemas.Test, tags=["authors"])
 def create_test(form_data: schemas.TestCreate, db: Session = Depends(get_db)):
     test = crud.create_test(db=db, title=form_data.title)
     return {"id":test.id, "title": test.title}
 
-@router.get("/")
+
+@router.get("/stages/", tags=["authors"])
 async def read_stages(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
     stages = crud.get_stages(db, skip=skip, limit=limit)
     return stages
 
 
-@router.get("/{stage_id}")
+@router.get("/stages/{stage_id}", tags=["authors"])
 async def read_stages(stage_id: int, db: Session = Depends(get_db)):
     stage = crud.get_stage(stage_id, db)
     return stage
