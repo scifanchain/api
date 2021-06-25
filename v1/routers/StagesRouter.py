08d789cc68b689b11e422025255df0cf4043981a
unity <unityoxb@gmail.com>
2021-06-25 19:52:42 +0800

    update

--- v1/routers/StagesRouter.py
+++ v1/routers/StagesRouter.py
@@ -9,24 +9,24 @@ from datapools.database import get_db
 router = APIRouter()
 
 
-@router.post("/stages/create_stage/", response_model=schemas.Stage, tags=["authors"])
+@router.post("/stages/create_stage/", response_model=schemas.Stage, tags=["stages"])
 async def create_stage(stage: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db), author: schemas.Author = Depends(crud.get_current_user)):
     return crud.create_stage(stage=stage, db=db, author=author)
 
 
-@router.post("/stages/test/", response_model=schemas.Test, tags=["authors"])
+@router.post("/stages/test/", response_model=schemas.Test, tags=["stages"])
 def create_test(form_data: schemas.TestCreate, db: Session = Depends(get_db)):
     test = crud.create_test(db=db, title=form_data.title)
     return {"id":test.id, "title": test.title}
 
 
-@router.get("/stages/", tags=["authors"])
+@router.get("/stages/", tags=["stages"])
 async def read_stages(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
     stages = crud.get_stages(db, skip=skip, limit=limit)
     return stages
 
 
-@router.get("/stages/{stage_id}", tags=["authors"])
+@router.get("/stages/{stage_id}", tags=["stages"])
 async def read_stages(stage_id: int, db: Session = Depends(get_db)):
     stage = crud.get_stage(stage_id, db)
     return stage
