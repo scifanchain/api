cb629167933ccddcac45b08be8724d94374bb57a
unity <unityoxb@gmail.com>
2021-07-05 00:15:56 +0800

    映射

--- v1/routers/StagesRouter.py
+++ v1/routers/StagesRouter.py
@@ -31,9 +31,8 @@ async def read_stages(stage_id: int, db: Session = Depends(get_db)):
 
 @router.put("/stages/{stage_id}", response_model=schemas.Stage, tags=["stages"])
 def update_stage(stage_id: int, stage_update: schemas.StageUpdate, db: Session = Depends(get_db), author: schemas.Author = Depends(crud.get_current_user)):
-    new_stage = crud.update_stage(stage_id, stage_update, db)
+    new_stage = crud.update_stage(stage_id=stage_id, stage_update=stage_update, db=db, author=author)
     if new_stage is None:
         raise HTTPException(status_code=404, detail="Stage not found")
-    new_stage.partners.append(author)
 
     return update_stage
